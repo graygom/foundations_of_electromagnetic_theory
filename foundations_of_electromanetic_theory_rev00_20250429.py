dac3d146d068e17d8b6ad8d2f1989d70cddfc9bc
Hyunseung Yoo <hyunseung.yoo.hs@gmail.com>
2025-05-06 22:05:43 +0900

    Update foundations_of_electromanetic_theory_rev00_20250429.py

--- foundations_of_electromanetic_theory_rev00_20250429.py
+++ foundations_of_electromanetic_theory_rev00_20250429.py
@@ -8,16 +8,17 @@
 
 
 import sys
-import time 
+import time
+import pickle
 import numpy as np
-#from numba import jit
+import scipy as sp
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
 
 #==============================================
 # class ES2D
-# example 3-5. potential in retangular region
+# REFERENCE = example 3-5. potential in retangular region
 #
 # Poisson equation: ∇·( ε(x,y) * ∇φ(x,y) ) = -ρ(x,y),
 # if ρ(x,y) = 0, then Laplace equation ∇·( ε(x,y) * ∇φ(x,y) ) = 0.0,
@@ -39,8 +40,23 @@ import matplotlib.pyplot as plt
 class ES2D:
 
     def __init__(self):
+        # geometry
+        self.mat = []
+        
+        # bias conditions
+        self.bias = []
+        
         # electric potential
         self.phi = []
+        
+        # electric field
+        self.ex = []
+        self.ey = []
+        self.em = []
+
+        # finite difference method
+        self.w = 0.0
+        self.conv_error = 0.0
 
         # speed of convergence
         self.iteration_no = 0
@@ -170,59 +186,60 @@ class ES2D:
         gate_major_cd = unit_cell_info['gate']['major_CD']
         gate_distortion = unit_cell_info['gate']['distortion']
 
-        # calculations
+        # calculations 1: geometry
         gate_minor_cd = gate_major_cd * gate_distortion
-
         onoa_thk = np.sum([ponoa_alo_thk, ponoa_box_thk, ponoa_ctn_thk, ponoa_tox_thk]) 
         pch_major_cd1 = gate_major_cd - 2.0 * onoa_thk
         pch_minor_cd1 = gate_minor_cd - 2.0 * onoa_thk
         pch_major_cd2 = pch_major_cd1 - 2.0 * ponoa_pch_thk_major
         pch_minor_cd2 = pch_minor_cd1 - 2.0 * ponoa_pch_thk_minor
 
+        # calculations 2: FDM calculation
         self.dx = (coordi_x_upper_limit - coordi_x_lower_limit) / coordi_x_elements          # element length
         self.dy = (coordi_y_upper_limit - coordi_y_lower_limit) / coordi_y_elements          # element length
 
-        self.x_nodes = int(coordi_x_elements + 1)        # number of nodes
-        self.y_nodes = int(coordi_y_elements + 1)        # number of nodes
+        self.x_nodes = int(coordi_x_elements + 1)                                           # number of nodes
+        self.y_nodes = int(coordi_y_elements + 1)                                           # number of nodes
         
-        x_range = np.linspace(coordi_x_lower_limit, coordi_x_upper_limit, self.x_nodes, dtype=np.float64)
-        y_range = np.linspace(coordi_y_lower_limit, coordi_y_upper_limit, self.y_nodes, dtype=np.float64)
+        self.x_range = np.linspace(coordi_x_lower_limit, coordi_x_upper_limit, self.x_nodes, dtype=np.float64)
+        self.y_range = np.linspace(coordi_y_lower_limit, coordi_y_upper_limit, self.y_nodes, dtype=np.float64)
 
-        x_range += 1e-9                                                 # avoiding numerical error
-        y_range += 1e-9                                                 # avoiding numerical error
+        self.x_range += 1e-9                                                # avoiding numerical error
+        self.y_range += 1e-9                                                # avoiding numerical error
 
-        coordi_x, coordi_y = np.meshgrid(x_range, y_range)              # making meshgrid
+        coordi_x, coordi_y = np.meshgrid(self.x_range, self.y_range)        # making meshgrid
 
-        # electric potential
+        # electric potential (initialization, averaging)
         self.phi  = np.ones([self.x_nodes, self.y_nodes], dtype=np.float64)
         self.phi *= (unit_cell_info['bias']['gate'] + unit_cell_info['bias']['pch_e'] + unit_cell_info['bias']['pch_o']) / 3.0
 
-        # gate
+        # gate region & bias
         gate_major_cd_half = gate_major_cd / 2.0
         gate_minor_cd_half = gate_minor_cd / 2.0
         
         inside_gate = ( (coordi_x / gate_major_cd_half)**2 + (coordi_y / gate_minor_cd_half)**2 ) <  1.0    # region
         self.bias = (inside_gate == 0.0) * unit_cell_info['bias']['gate']                                   # bias
 
-        # pch
+        # pch region & bias
         pch_major_cd1_half = pch_major_cd1 / 2.0
         pch_minor_cd1_half = pch_minor_cd1 / 2.0
-        pch1 = ( (coordi_x / pch_major_cd1_half)**2 + (coordi_y / pch_minor_cd1_half)**2 ) <  1.0       # region
+        pch1 = ( (coordi_x / pch_major_cd1_half)**2 + (coordi_y / pch_minor_cd1_half)**2 ) <  1.0       # region 1
 
         pch_major_cd2_half = pch_major_cd2 / 2.0
         pch_minor_cd2_half = pch_minor_cd2 / 2.0
-        pch2 = ( (coordi_x / pch_major_cd2_half)**2 + (coordi_y / pch_minor_cd2_half)**2 ) >  1.0       # region
+        pch2 = ( (coordi_x / pch_major_cd2_half)**2 + (coordi_y / pch_minor_cd2_half)**2 ) >  1.0       # region 2
 
         ch_cut_cd_half = ch_cut_cd / 2.0
-        ch_cut = np.abs(coordi_x) > ch_cut_cd_half      # region
+        ch_cut = np.abs(coordi_x) > ch_cut_cd_half                                                      # region 3
 
-        pch = pch1 & pch2 & ch_cut                                                              # region
-        self.bias += ((pch == 1.0) & (coordi_x < 0)) * unit_cell_info['bias']['pch_e']            # bias
-        self.bias += ((pch == 1.0) & (coordi_x > 0)) * unit_cell_info['bias']['pch_o']            # bias
+        pch = pch1 & pch2 & ch_cut                                                                      # region 1 & 2 & 3
+        
+        self.bias += ((pch == 1.0) & (coordi_x < 0)) * unit_cell_info['bias']['pch_e']                  # bias
+        self.bias += ((pch == 1.0) & (coordi_x > 0)) * unit_cell_info['bias']['pch_o']                  # bias
 
-        #  
-        self.dielectric = inside_gate & (pch == 0)      # region
-        self.mat = self.dielectric.copy()               # region
+        # FDM calculation region
+        self.dielectric = inside_gate & (pch == 0)      # region = inside gate & outside channel
+        self.mat = self.dielectric.copy()               # region where FDM calculations are run
 
         # debugging
         if False:
@@ -232,7 +249,7 @@ class ES2D:
             plt.show()
 
 
-    def finite_difference_method2(self, overrelaxation, conv_error):
+    def before_finite_difference_method(self, overrelaxation, conv_error):
         #
         self.w = overrelaxation
         
@@ -243,15 +260,21 @@ class ES2D:
 
         #
         self.conv_error = conv_error
+
+
+    def finite_difference_method2(self):
+        #
+        print('FDM iteration strat time = %s' % time.ctime())
         
         #
+        start_time = time.time()
         error = 1.0
 
         while error > self.conv_error:
-            #
+            # error check reference
             prev_phi = self.phi.copy()
             
-            # iterations
+            # FDM iterations using numpy array operations
             self.phi[1:-1,1:-1] = self.x_coeff * ( self.phi[2:,1:-1] + self.phi[:-2,1:-1] ) + \
                                   self.y_coeff * ( self.phi[1:-1,2:] + self.phi[1:-1,:-2] ) + \
                                   self.conv_coeff * self.phi[1:-1,1:-1]
@@ -259,43 +282,182 @@ class ES2D:
             # Dirichlet boundary conditions
             self.phi = np.where(self.mat == 0.0, self.bias, self.phi)
             
-            # error
+            # calculating error
             error = np.max( np.abs(prev_phi - self.phi) )
 
             # debugging
             if self.iteration_no % 1000 == 0:
-                output_string = 'iteration = %i, error = %.2e, time = %s' % (self.iteration_no, error, time.ctime())
+                elapsed_time = time.time() - start_time
+                output_string = 'iteration = %i, error = %.2e, elapsed time = %.1fmin' % (self.iteration_no, error, elapsed_time/60.0)
                 print(output_string)
 
-            # iteration no.
+            # calculating iteration numnber
             self.iteration_no += 1
 
         #
-        print( 'error = %.2e, iteration = %i ea' % (conv_error, self.iteration_no) )
+        print( 'target error = %.2e, iterations = %i ea' % (self.conv_error, self.iteration_no) )
 
         #
-        print('iteration end time = %s' % time.ctime())
+        print('FDM iteration end time = %s' % time.ctime())
 
-        # electric field
+        # calculating electric field
         self.ex = np.gradient(self.phi, axis=0)
         self.ey = np.gradient(self.phi, axis=1)
         self.em = np.sqrt(self.ex*self.ex + self.ey*self.ey)
+
+        # Dirichlet boundary conditions (post-processing metal region)
+        self.ex = np.where(self.mat == 0.0, 0.0, self.ex)
+        self.ey = np.where(self.mat == 0.0, 0.0, self.ey)
+        self.em = np.where(self.mat == 0.0, 0.0, self.em)
+
+
+    def finite_difference_method_error_check(self):
+        # error check reference
+        prev_phi = self.phi.copy()
+            
+        # FDM iteration 1ea
+        for x_node_cnt in range(self.x_nodes):
+            for y_node_cnt in range(self.y_nodes):
+                # material check: dielectrics
+                if self.mat[x_node_cnt,y_node_cnt] != 0:
+                    self.phi[x_node_cnt,y_node_cnt] = self.x_coeff * (self.phi[x_node_cnt+1,y_node_cnt] + self.phi[x_node_cnt-1,y_node_cnt]) + \
+                                                      self.y_coeff * (self.phi[x_node_cnt,y_node_cnt+1] + self.phi[x_node_cnt,y_node_cnt-1]) + \
+                                                      self.conv_coeff * self.phi[x_node_cnt,y_node_cnt]
+
+        # calculating error
+        error = np.max( np.abs(prev_phi - self.phi) )
+        print( 'finite_difference_method_error_check(), checked error = %.2e' % error )
+
+
+    def save_FDM_result(self, output_filename):
+        #
+        fid = open(output_filename, 'wb')
+
+        #
+        pickle.dump([self.mat, self.bias, self.phi, self.ex, self.ey, self.em, self.w, self.conv_error, self.iteration_no], fid)
+
+        #
+        fid.close()
+
+
+    def load_FDM_result(self, input_filename):
+        #
+        fid = open(input_filename, 'rb')
+
+        #
+        self.mat, self.bias, self.phi, self.ex, self.ey, self.em, self.w, self.conv_error, self.iteration_no = pickle.load(fid)
+
+        #
+        fid.close()
+
+
+    def analysis(self):
+        #
+        x_center_index = int( (self.x_nodes - 1) / 2.0 )
+        y_center_index = int( (self.y_nodes - 1) / 2.0 )
+
+        #
+        phi_at_x_center = self.phi[x_center_index, :]
+        phi_at_y_center = self.phi[:, y_center_index]
+
+        #
+        em_at_x_center = self.em[x_center_index, :]
+        em_at_y_center = self.em[:, y_center_index]
+
+        #
+        coordi_x, coordi_y = np.meshgrid(self.x_range, self.y_range) 
+
+        # text information
+        fdm_nodes = np.sum(np.where(self.mat != 0.0, 1.0, 0.0))
+        dirichlet_nodes = np.sum(np.where(self.mat == 0.0, 1.0, 0.0))
+        print('Number of FDM nodes = %i ea' % fdm_nodes)
+        print('Number of Dirichlet nodes = %i ea' % dirichlet_nodes)
+
+        # visualization 1
+        fig, ax = plt.subplots(1, 1)
+        #
+        plt.imshow(self.mat, cmap=cm.jet)
+        plt.grid(ls=':')
+        plt.colorbar()
+        plt.title('Materials')
+        plt.savefig('figure_1_mat.pdf')
+
+        # visualization 2
+        fig, ax = plt.subplots(1, 1)
+        #
+        plt.imshow(self.bias, cmap=cm.jet)
+        plt.grid(ls=':')
+        plt.colorbar()
+        plt.title('Bias')
+        plt.savefig('figure_2_bias.pdf')
+
+        # visualization 3
+        fig, ax = plt.subplots(1, 1)
+        #
+        CS1 = plt.contour(coordi_x, coordi_y, self.phi, levels=15, cmap=cm.jet)
+        plt.grid(ls=':')
+        plt.colorbar()
+        plt.title('Electric Potential')
+        plt.savefig('figure_3_phi.pdf')
+
+        # visualization 4
+        fig, ax = plt.subplots(1, 1)
+        #
+        CS2 = plt.contour(coordi_x, coordi_y, self.em, levels=30, cmap=cm.jet)
+        plt.grid(ls=':')
+        plt.colorbar()
+        plt.title('Electric Field Magnitude')
+        plt.savefig('figure_4_em.pdf')
+
+        # visualization 5
+        fig, ax = plt.subplots(1, 1)
+        #
+        CSF1 = plt.contourf(coordi_x, coordi_y, self.phi, levels=10, cmap=cm.jet)   # cmap="RdBu_r"
+        plt.grid(ls=':')
+        plt.colorbar()
+        plt.title('Electric Potential')
+        plt.savefig('figure_5_phi_f.pdf')
+
+        # visualization 6
+        fig, ax = plt.subplots(1, 1)
+        #
+        CSF2 = plt.contourf(coordi_x, coordi_y, self.em, levels=100, cmap=cm.jet)   # cmap="RdBu_r"
+        plt.grid(ls=':')
+        plt.colorbar()
+        plt.title('Electric Field Magnitude')
+        plt.savefig('figure_6_em_f.pdf')
+
+        plt.close()
+        
+
         
 
 #==============================================
 # main
 #
 
+geometry_info = {}
+geometry_info['x'] = {'length':11.0, 'elements':110}            # in m, ea
+geometry_info['y'] = {'length':6.0,  'elements':60}             # in m, ea
+
+bias_conditions = {}
+bias_conditions['x'] = {'left':0.3, 'right':0.7}
+bias_conditions['y'] = {'upper':1.0, 'lower':0.0}
+
+unit_cell_info = {}
+unit_cell_info['coordi_x_limit'] = {'lower':-1000.0, 'upper':1000.0, 'elements': 2000}
+unit_cell_info['coordi_y_limit'] = {'lower':-1000.0, 'upper':1000.0, 'elements': 2000}
+unit_cell_info['ponoa_alo'] = {'thk':[30.0], 'k':9.0}
+unit_cell_info['ponoa_box'] = {'thk':[70.0], 'k':4.5}
+unit_cell_info['ponoa_ctn'] = {'thk':[54.0], 'k':7.5}
+unit_cell_info['ponoa_tox'] = {'thk':[48.0], 'k':5.0}
+unit_cell_info['ponoa_pch'] = {'thk':[70.0, 60.0], 'k':11.7}
+unit_cell_info['gate'] = {'major_CD':1500.0, 'distortion':0.7}
+unit_cell_info['cut_cd'] = 540.0
+unit_cell_info['bias'] = {'gate':7.0, 'pch_e':0.0, 'pch_o':0.0}
+
 # text example
 if False:
-    geometry_info = {}
-    geometry_info['x'] = {'length':11.0, 'elements':110}         # in m, ea
-    geometry_info['y'] = {'length':6.0,  'elements':60}         # in m, ea
-
-    bias_conditions = {}
-    bias_conditions['x'] = {'left':0.3, 'right':0.7}
-    bias_conditions['y'] = {'upper':1.0, 'lower':0.0}
-
     ex3_5 = ES2D()
     ex3_5.make_rectangular_box(geometry_info)
     ex3_5.dirichlet_boundary_conditions(bias_conditions)
@@ -303,29 +465,36 @@ if False:
 
 # silicon work
 if True:
-    unit_cell_info = {}
-    unit_cell_info['coordi_x_limit'] = {'lower':-1000.0, 'upper':1000.0, 'elements': 2000}
-    unit_cell_info['coordi_y_limit'] = {'lower':-1000.0, 'upper':1000.0, 'elements': 2000}
-    unit_cell_info['ponoa_alo'] = {'thk':[30.0], 'k':9.0}
-    unit_cell_info['ponoa_box'] = {'thk':[70.0], 'k':4.5}
-    unit_cell_info['ponoa_ctn'] = {'thk':[54.0], 'k':7.5}
-    unit_cell_info['ponoa_tox'] = {'thk':[48.0], 'k':5.0}
-    unit_cell_info['ponoa_pch'] = {'thk':[70.0, 60.0], 'k':11.7}
-    unit_cell_info['gate'] = {'major_CD':1500.0, 'distortion':0.7}
-    unit_cell_info['cut_cd'] = 540.0
-    unit_cell_info['bias'] = {'gate':7.0, 'pch_e':0.0, 'pch_o':0.0}
-
     silicon = ES2D()
     silicon.make_unit_cell_geometry(unit_cell_info)
-    silicon.finite_difference_method2(overrelaxation=0.2, conv_error=1e-4)
+    
+    if False:
+        silicon.before_finite_difference_method(overrelaxation=0.2, conv_error=1e-5)
+        silicon.finite_difference_method2()
+        silicon.finite_difference_method_error_check()
+        silicon.save_FDM_result(output_filename='FDM_result_150nm_0.7_cut_54nm_onoa_30_70_54_46_pch_70_60.pkl')
+
+    if False:
+        silicon.load_FDM_result(input_filename='FDM_result_150nm_0.7_cut_54nm_onoa_30_70_54_46_pch_70_60_8e-6.pkl')
+        silicon.before_finite_difference_method(overrelaxation=0.2, conv_error=5e-6)
+        silicon.finite_difference_method2()
+        silicon.finite_difference_method_error_check()
+        silicon.save_FDM_result(output_filename='FDM_result_150nm_0.7_cut_54nm_onoa_30_70_54_46_pch_70_60_5e-6.pkl')
+        silicon.analysis()
+
+    if True:
+        silicon.load_FDM_result(input_filename='FDM_result_150nm_0.7_cut_54nm_onoa_30_70_54_46_pch_70_60_5e-6.pkl')
+        silicon.analysis()
 
 # visualization
-if True:
+if False:
     fig, ax = plt.subplots(2, 2)
     ax[0,0].imshow(silicon.ex)
     ax[0,1].imshow(silicon.ey)
     ax[1,0].imshow(silicon.em)
     ax[1,1].imshow(silicon.phi)
     plt.show()
+
+
 
 
